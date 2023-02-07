4fed39dc04e1bea34dc00fc1a9c847cfd8d424af
Samuel Evans <117552758+JSamuel38@users.noreply.github.com>
2023-02-07 17:28:13 +0000

    Set up weather API fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+ 
 function App() {
+  const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=1.70&longitude=-1.30&hourly=temperature_2m&hourly=rain&hourly=snowfall&hourly=windspeed_10m&hourly=cloudcover';
+
+  const [location, setLocation] = useState(API_URL);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(API_URL);
+      const locationData = await response.json();
+      console.log(locationData);
+    };
+    try {
+      getData();
+    } catch (err) {
+      console.error(err);
+    }
+  }, [location]);
+
   return (
     <div className="App">
-      
+      hello
     </div>
   );
 }
